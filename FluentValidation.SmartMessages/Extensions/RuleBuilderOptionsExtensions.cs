43a48914a3890021a844915024e18110df12a720
vludlss <vladislav.mikkoev@yandex.ru>
2024-02-24 11:17:03 +0300

    feat: Добавить метод When (выражение)
    Позволяет переопределять сообщения предыдущих валидаторов в одном месте, с сохранением прошлого текста (вторая перегрузка)

--- FluentValidation.SmartMessages/Extensions/RuleBuilderOptionsExtensions.cs
+++ FluentValidation.SmartMessages/Extensions/RuleBuilderOptionsExtensions.cs
@@ -23,5 +23,25 @@ namespace FluentValidation.SmartMessages.Extensions
                 }
             });
         }
+
+        public static IRuleBuilderOptions<T, TProperty> When<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule, Func<T, ValidationContext<T>, bool> predicate, Func<string, string> func, ApplyConditionTo applyConditionTo = ApplyConditionTo.AllValidators)
+        {
+            return rule.When(predicate, applyConditionTo).Configure(cfg =>
+            {
+                if (applyConditionTo == ApplyConditionTo.AllValidators)
+                {
+                    foreach (IRuleComponent<T, TProperty> component in cfg.Components)
+                    {
+                        var previousMessage = component.GetUnformattedErrorMessage();
+                        component.SetErrorMessage(func(previousMessage));
+                    }
+                }
+                else
+                {
+                    var previousMessage = cfg.Current.GetUnformattedErrorMessage();
+                    cfg.Current.SetErrorMessage(func(previousMessage));
+                }
+            });
+        }
     }
 }
