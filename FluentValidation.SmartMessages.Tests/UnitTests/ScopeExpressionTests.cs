e933d74e4c15b44d259c56ca16aa461f2eeae131
vludlss <vladislav.mikkoev@yandex.ru>
2024-02-25 07:37:34 +0300

    test: Добавить прототипы тестов для метода Scope (expression)

---
+++ FluentValidation.SmartMessages.Tests/UnitTests/ScopeExpressionTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions.Common;
+using FluentValidation.SmartMessages.Enums;
+using FluentValidation.SmartMessages.Extensions;
+using FluentValidation.TestHelper;
+
+namespace FluentValidation.SmartMessages.Tests.UnitTests
+{
+    public class ScopeExpressionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var validator = new ScopeExpressionModelValidator();
+            var model = new ScopeExpressionModel()
+            {
+                FirstName = "test"
+            };
+
+            var result = validator.TestValidate(model);
+        }
+
+        public class ScopeExpressionModelValidator : SmartValidator<ScopeExpressionModel>
+        {
+            public ScopeExpressionModelValidator()
+            {
+                RuleFor(model => model.FirstName).MaximumLength(3)
+                    .Equal("str")
+                    .Scope(msg => $"{msg} Просто область", ApplyScopeTo.CurrentValidator);
+            }
+        }
+
+        public class ScopeExpressionModel
+        {
+            public string FirstName { get; set; }
+            public StartTimer SecondName { get; set; }
+        }
+    }
+}
---
+++ FluentValidation.SmartMessages.Tests/UnitTests/ScopeTopLevelTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.SmartMessages.Extensions;
+using FluentValidation.TestHelper;
+
+namespace FluentValidation.SmartMessages.Tests.UnitTests
+{
+    public class ScopeTopLevelTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var validator = new ScopeModelValidator();
+            var model = new ScopeModel();
+
+            var result = validator.TestValidate(model);
+        }
+
+        public class ScopeModelValidator : SmartValidator<ScopeModel>
+        {
+            public ScopeModelValidator()
+            {
+                Scope(bag =>
+                {
+                    RuleFor(model => model.FirstName).NotNull().AttachTo(bag);
+                    RuleFor(model => model.SecondName).NotNull().AttachTo(bag);
+                })
+                .WithMessage(msg => $"{msg} Просто область");
+            }
+        }
+
+        public class ScopeModel
+        {
+            public string FirstName { get; set; }
+            public string SecondName { get; set; }
+        }
+    }
+}
